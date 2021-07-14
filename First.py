5aa48eb2329a68f0466d22d81d649df36f11798d
01Krishna <87380877+01Krishna@users.noreply.github.com>
2021-07-14 07:33:33 +0530

    Create First.py

---
+++ First.py
@@ -0,0 +1,4 @@
+print("******************Welcome*****************")
+name = input("Enter your name:- ")
+phone no = input("Enter your contact number:- ")
+print(f"Hello mr {name} Your number is {phone no}")
